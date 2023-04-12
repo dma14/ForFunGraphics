b2cce87d37aa327574d251154823d8b4a89d4a3f
David Ma <davidcwma14@gmail.com>
2023-04-12 19:33:11 -0400

    Improve performance for drawing large faces by drawing lines on-the-fly.

--- ForFunGraphics/Scene.cpp
+++ ForFunGraphics/Scene.cpp
@@ -14,14 +14,23 @@ void Scene::Render(ID2D1HwndRenderTarget* RenderTarget, ID2D1SolidColorBrush *Br
     D2D1_POINT_2F SpaceMax = { 1., 1. };
     D2D1_POINT_2F WindowMin = { 0., 0. };
     D2D1_POINT_2F WindowMax = { WindowSize.width, WindowSize.height };
-    for (Object &Obj : Objects) {
+    for (Object& Obj : Objects) {
+        Coord Displacement = Obj.GetDisplacement();
+        Coord PrevVertex, Vertex = { 0 };
+        D2D1_POINT_2F PrevVertex2D, Vertex2D = { 0 };
+        bool PrevSkipVertex, SkipVertex = false;
         for (Face& F : Obj.GetFaces()) {
-            Coord Displacement = Obj.GetDisplacement();
+
+            // Handle adjacent vertices in pairs, so we can draw the outline. To do this we need
+            // to loop one extra time.
             vector<Coord> Vertices = Obj.GetVerticesForFace(F);
-            vector<D2D1_POINT_2F> Vertices2D(Vertices.size());
-            vector<bool> SkipDrawingVertex(Vertices.size(), false);
-            for (unsigned Idx = 0; Idx < Vertices.size(); Idx++) {
-                Coord Vertex = Vertices.at(Idx);
+            for (unsigned Idx = 0; Idx < Vertices.size() + 1; Idx++) {
+                // Current vertex is now the previous vertex
+                PrevVertex = Vertex;
+                PrevVertex2D = Vertex2D;
+                PrevSkipVertex = SkipVertex;
+
+                Vertex = Vertices.at(Idx % Vertices.size());
                 Coord VertexRelative, VertexRotated, VertexFovCone;
                 // Translate the object to match with the camera location, as well as any
                 // inherent translation.
@@ -34,7 +43,7 @@ void Scene::Render(ID2D1HwndRenderTarget* RenderTarget, ID2D1SolidColorBrush *Br
 
                 // Check if the vertex is between the (real) Z planes we wish to draw
                 if (VertexRotated.Z > ZNear || VertexRotated.Z < ZFar) {
-                    SkipDrawingVertex[Idx] = true;
+                    SkipVertex = true;
                     continue;
                 }
                 
@@ -54,29 +63,25 @@ void Scene::Render(ID2D1HwndRenderTarget* RenderTarget, ID2D1SolidColorBrush *Br
                 // Check if the vertex is within the FoV, if not we shouldn't draw it...
                 if (VertexFovCone.X < -1.f || VertexFovCone.X > 1.f ||
                     VertexFovCone.Y < -1.f || VertexFovCone.Y > 1.f) {
-                    SkipDrawingVertex[Idx] = true;
+                    SkipVertex = true;
                     continue;
                 }
 
                 // Now convert the FoV cone into screen coordinates
-                D2D1_POINT_2F Vertex2D =
-                    Convert2DCoords({ VertexFovCone.X, VertexFovCone.Y },
-                                    SpaceMin,
-                                    SpaceMax,
-                                    WindowMin,
-                                    WindowMax);
+                Vertex2D = Convert2DCoords({ VertexFovCone.X, VertexFovCone.Y },
+                                           SpaceMin,
+                                           SpaceMax,
+                                           WindowMin,
+                                           WindowMax);
                 // Real space is bottom-left oriented, while the window coords are top-left
                 // oriented.
                 Vertex2D.y = WindowSize.height - Vertex2D.y;
-                Vertices2D.at(Idx) = Vertex2D;
-            }
 
-            // Draw each line (that we should) that makes up the face
-            for (unsigned Idx = 0; Idx < Vertices2D.size(); Idx++) {
-                unsigned NextIdx = (Idx + 1) % Vertices2D.size();
-                if (!SkipDrawingVertex[Idx] && !SkipDrawingVertex[NextIdx])
+                // Draw a line between the vertices, just not for the first iteration.
+                if (Idx != 0 && !PrevSkipVertex && !SkipVertex) {
                     RenderTarget->DrawLine(
-                        Vertices2D[Idx], Vertices2D[NextIdx], Brush, StrokeWidth);
+                        PrevVertex2D, Vertex2D, Brush, StrokeWidth);
+                }
             }
         }
     }
